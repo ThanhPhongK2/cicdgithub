2f6fb1dc3cc9e587515400d8a42bb05ce9e6635a
Phong <phong044756@gmail.com>
2025-07-16 10:39:57 +0700

    Finish merge 3

--- frontend/src/components/VerticalCardProduct.js
+++ frontend/src/components/VerticalCardProduct.js
@@ -76,9 +76,13 @@ const VerticalCardProduct = ({category, heading}) => {
                 ) : (
                     data.map((product,index)=>{
                         return(
-                            <Link to={"product/"+product?._id} className='w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow '>
+                            <Link to={"product/"+product?._id} className='productCard w-full min-w-[280px] md:min-w-[320px] max-w-[320px]'>
                                 <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center'>
-                                    <img src={product.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply'/>
+                                    <img
+                                        src={product.productImage[0]}
+                                        alt={product.productName}
+                                        className='object-contain h-full transition-transform duration-300 ease-in-out hover:scale-110 mix-blend-multiply'
+                                    />
                                 </div>
                                 <div className='p-4 grid gap-3'>
                                     <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black'>{product?.productName}</h2>
