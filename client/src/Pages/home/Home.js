579da229ad9d4b43b9c003ac1ce2c952a993dbf7
Lê Đình Thanh Phong <ledinhthanhphong2k2@gmail.com>
2025-08-21 15:04:30 +0700

    update

--- client/src/Pages/home/Home.js
+++ client/src/Pages/home/Home.js
@@ -1,33 +1,17 @@
-import React from "react";
-import "./Home.css";
-import ProfileSide from "../../Components/profileSide/ProfileSide";
-import PostSide from "../../Components/PostSide/PostSide";
-import RightSide from "../../Components/RightSide/RightSide";
+import React from 'react'
+import './Home.css'
+import ProfileSide from '../../Components/profileSide/ProfileSide'
+import PostSide from '../../Components/PostSide/PostSide'
+import RightSide from '../../Components/RightSide/RightSide'
 
 const Home = () => {
   return (
     <div className="Home">
-      <div className="home-bg">
-        {/* Blob gradient background */}
-        <div className="blob blob-purple"></div>
-        <div className="blob blob-blue"></div>
-      </div>
-
-      <div className="home-container">
-        <aside className="home-sidebar card glass-card">
-          <ProfileSide />
-        </aside>
-
-        <main className="home-feed card glass-card">
-          <PostSide />
-        </main>
-
-        <aside className="home-rightbar card glass-card">
-          <RightSide />
-        </aside>
-      </div>
+        <ProfileSide />
+        <PostSide />
+        <RightSide />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
