6d1de6eecc9212e30612292e0072a742ab17bb1d
Lê Đình Thanh Phong <ledinhthanhphong2k2@gmail.com>
2025-08-21 14:57:05 +0700

    update

--- client/src/Pages/home/Home.js
+++ client/src/Pages/home/Home.js
@@ -7,18 +7,22 @@ import RightSide from "../../Components/RightSide/RightSide";
 const Home = () => {
   return (
     <div className="Home">
-      <div className="home-bg" />
+      <div className="home-bg">
+        {/* Blob gradient background */}
+        <div className="blob blob-purple"></div>
+        <div className="blob blob-blue"></div>
+      </div>
 
       <div className="home-container">
-        <aside className="home-sidebar card">
+        <aside className="home-sidebar card glass-card">
           <ProfileSide />
         </aside>
 
-        <main className="home-feed card">
+        <main className="home-feed card glass-card">
           <PostSide />
         </main>
 
-        <aside className="home-rightbar card">
+        <aside className="home-rightbar card glass-card">
           <RightSide />
         </aside>
       </div>
