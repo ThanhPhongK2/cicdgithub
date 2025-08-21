4afc741a7d7111d4329dddc27cce4ff7bbdcf293
Lê Đình Thanh Phong <ledinhthanhphong2k2@gmail.com>
2025-08-21 14:04:01 +0700

    update

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,9 +1,13 @@
+// jest.config.js
 module.exports = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.[tj]sx?$": "babel-jest"
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(axios)/)" // 👈 ép Jest transpile axios
-  ]
+    "node_modules/(?!(axios)/)" // ép Jest transpile axios
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  }
 };
