4515e147c99849f3fb1f69525dd2aa4e9dc344b8
Lê Đình Thanh Phong <ledinhthanhphong2k2@gmail.com>
2025-08-21 11:28:18 +0700

    update

--- client/src/Components/PostShare/PostShare.js
+++ client/src/Components/PostShare/PostShare.js
@@ -1,125 +1,133 @@
-import React, { useState, useRef } from 'react'
-import './PostShare.css';
-import PhotoOutlinedIcon from '@mui/icons-material/PhotoOutlined';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import { useDispatch, useSelector } from 'react-redux';
-import { uploadImage, uploadPost } from '../../actions/UploadAction';
-
-
+import React, { useState, useRef } from "react";
+import "./PostShare.css";
+import PhotoOutlinedIcon from "@mui/icons-material/PhotoOutlined";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { useDispatch, useSelector } from "react-redux";
+import { uploadImage, uploadPost } from "../../actions/UploadAction";
 
 const PostShare = () => {
+  const loading = useSelector((state) => state.postReducer.uploading);
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+  const desc = useRef();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
 
-    const loading = useSelector((state) => state.postReducer.uploading)
-    const [image, setImage] = useState(null);
-    const imageRef = useRef();
-    const dispatch = useDispatch();
-    const desc = useRef();
-    const { user } = useSelector((state) => state.authReducer.authData);
-    const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(event.target.files[0]);
+    }
+  };
 
+  const reset = () => {
+    setImage(null);
+    if (desc.current) desc.current.value = "";
+    if (imageRef.current) imageRef.current.value = null;
+  };
 
-    const onImageChange = (event) => {
-        if (event.target.files && event.target.files[0]) {
-            let img = event.target.files[0];
-            setImage(img);
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!desc.current.value.trim() && !image) {
+      return; // không cho đăng bài rỗng
     }
 
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
 
+    try {
+      if (image) {
+        const data = new FormData();
+        const filename = Date.now() + image.name;
+        data.append("name", filename);
+        data.append("file", image);
 
+        newPost.image = filename;
 
-    const reset = () => {
-        setImage(null);
-        desc.current.value = ""
+        await dispatch(uploadImage(data));
+      }
+
+      await dispatch(uploadPost(newPost));
+      reset();
+    } catch (error) {
+      console.error("Error uploading post:", error);
     }
+  };
 
-
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const newPost = {
-            userId: user._id,
-            desc: desc.current.value,
+  return (
+    <div className="PostShare">
+      <img
+        src={
+          user.profilePicture
+            ? serverPublic + user.profilePicture
+            : serverPublic + "defaultProfile.png"
         }
+        alt="Profile"
+      />
 
-        if (image) {
-            const data = new FormData();
-            const filename = Date.now() + image.name;
-            data.append("name", filename);
-            data.append("file", image);
+      <form className="postForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Write a caption..."
+          required
+          ref={desc}
+        />
 
-            newPost.image = filename;
+        <div className="postOptions">
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            onClick={() => imageRef.current.click()}
+          >
+            <PhotoOutlinedIcon />
+            Photo
+          </div>
 
-            try {
-                dispatch(uploadImage(data))
-            } catch (error) {
-                console.log(error)
-            }
-        }
+          <div className="option" style={{ color: "var(--video)" }}>
+            <PlayCircleOutlineIcon />
+            Video
+          </div>
+          <div className="option" style={{ color: "var(--location)" }}>
+            <LocationOnOutlinedIcon />
+            Location
+          </div>
+          <div className="option" style={{ color: "var(--shedule)" }}>
+            <CalendarMonthOutlinedIcon />
+            Schedule
+          </div>
 
-        dispatch(uploadPost(newPost))
-        reset()
-    }
+          <button
+            type="submit"
+            className="button ps-button"
+            disabled={loading}
+          >
+            {loading ? "Uploading..." : "Share"}
+          </button>
 
-    return (
-        <div className="PostShare">
-            <img src={user.profilePicture ? serverPublic + user.profilePicture : serverPublic + "defaultProfile.png"} alt="" />
-
-            <div>
-                <input type="text" placeholder='Write a caption...' required ref={desc} />
-
-                <div className="postOptions">
-
-                    <div className="option" style={{ color: "var(--photo)" }}
-                        onClick={() => imageRef.current.click()}
-                    >
-                        <PhotoOutlinedIcon />
-                        Photo
-                    </div>
-
-                    <div className="option" style={{ color: "var(--video)" }}>
-                        <PlayCircleOutlineIcon />
-                        Video
-                    </div>
-                    <div className="option" style={{ color: "var(--location)" }}>
-                        <LocationOnOutlinedIcon />
-                        Location
-                    </div>
-                    <div className="option" style={{ color: "var(--shedule)" }}>
-                        <CalendarMonthOutlinedIcon />
-                        Shedule
-                    </div>
-
-                    <button className='button ps-button' onClick={handleSubmit} disabled={loading}>
-                        {loading ? "uploading..." : "Share"}
-                    </button>
-
-                    <div style={{ display: "none" }}>
-                        <input
-                            type="file"
-                            name='myImage'
-                            ref={imageRef}
-                            onChange={onImageChange}
-                        />
-                    </div>
-                </div>
-
-
-                {image && (
-                    <div className="previewImage">
-                        <CloseOutlinedIcon onClick={() => setImage(null)} />
-                        <img src={URL.createObjectURL(image)} alt="" />
-                    </div>
-                )}
-
-            </div>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
         </div>
-    )
-}
 
-export default PostShare
+        {image && (
+          <div className="previewImage">
+            <CloseOutlinedIcon onClick={() => setImage(null)} />
+            <img src={URL.createObjectURL(image)} alt="preview" />
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default PostShare;
