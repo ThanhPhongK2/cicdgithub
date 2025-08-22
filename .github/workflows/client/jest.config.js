31df6effcd5a105f95b00bbe49f37e33a5138565
Lê Đình Thanh Phong <ledinhthanhphong2k2@gmail.com>
2025-08-22 08:34:55 +0700

    update

--- .github/workflows/client/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.[tj]sx?$": "babel-jest"
-  },
-  transformIgnorePatterns: [
-    "node_modules/(?!(axios)/)" // 👈 ép Jest transpile axios
-  ]
-};
--- .github/workflows/client/src/api/AuthRequest.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://13.231.252.154:5000' });
-
-export const logIn = (formData) => API.post('/auth/login', formData); 
-
-export const signUp = (formData) => API.post('/auth/register', formData);
